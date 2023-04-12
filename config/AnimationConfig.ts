89ac7c8610430d0c3aad6f6913280ccf3733cfd8
Samantha Withanage <samantha@frostdigital.se>
2023-04-13 02:23:09 +0300

    Fix lint issues and update snapshots

--- config/AnimationConfig.ts
+++ config/AnimationConfig.ts
@@ -7,6 +7,5 @@ export const timingConfig: WithTimingConfig = {
   easing: Easing.bezier(0.5, 0.01, 0, 1),
 };
 
-
 export const LIST_REVEAL_ANIMATION_DELAY = 200;
 export const LIST_REVEAL_ANIMATION_STEP = 50;
